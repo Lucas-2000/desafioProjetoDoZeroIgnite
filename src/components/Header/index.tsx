ac734b89abdc90be9cded998bd3da222243a5eb4
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2021-12-07 15:38:41 -0300

    Projeto finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import Link from 'next/link';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
 }
